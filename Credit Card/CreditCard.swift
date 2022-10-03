6656fcf7a92eb0f00bbaf4cd6080fbfa4271a0e9
Oncu <oohancan@gmail.com>
2022-10-03 03:23:40 +0300

    add text and others for front

--- Credit Card/CreditCard.swift
+++ Credit Card/CreditCard.swift
@@ -17,11 +17,56 @@ struct CreditCard<Content>: View where Content: View {
 
 struct CreditCardFront: View {
     var body: some View {
-        VStack {
-            Text("FRONT")
+        VStack(alignment: .leading) {
+            HStack(alignment: .top) {
+                Image(systemName: "checkmark.circle.fill")
+                    .foregroundColor(.white)
+                Spacer()
+                Text("VISA")
+                    .foregroundColor(.white)
+                    .font(.system(size: 24))
+                    .fontWeight(.bold)
+                
+            }
+            Spacer()
+            
+            Text("**** **** **** 2964")
+                .font(.system(size: 32))
                 .foregroundColor(.white)
+            
+            Spacer()
+            
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("CARD HOLDER")
+                        .font(.caption)
+                        .fontWeight(.bold)
+                        .foregroundColor(.black)
+                    
+                    Text("ÖNCÜ CAN")
+                        .font(.caption)
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                    
+                }
+                Spacer()
+                
+                VStack {
+                    Text("EXPIRES")
+                        .font(.caption)
+                        .fontWeight(.bold)
+                        .foregroundColor(.black)
+                    
+                    Text("04/08")
+                        .font(.caption)
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                    
+                }
+            }
         }
                 .frame(width: 300, height: 200)
+                .padding()
                 .background(LinearGradient(gradient: Gradient(colors: [Color.yellow, Color.brown]), startPoint: .leading, endPoint: .trailing))
                 .cornerRadius(10)
     }
@@ -30,17 +75,29 @@ struct CreditCardFront: View {
 struct CreditCardBack: View {
     var body: some View {
         VStack {
-            Text("BACK")
-                .foregroundColor(.white)
+            Rectangle()
+                .frame(maxWidth: .infinity, maxHeight: 20).padding([.top])
+            Spacer()
+            
+            HStack {
+                Text("123")
+                    .foregroundColor(.black)
+                    .rotation3DEffect(.degrees(180),
+                                      axis: (x: 0.0, y: 1.0, z: 0.0))
+                    .padding(5)
+                    .frame(width: 100, height: 20)
+                    .background(.white)
+                Spacer()
+            }.padding()
         }
                 .frame(width: 300, height: 200)
-                .background(LinearGradient(gradient: Gradient(colors: [Color.gray, Color.brown]), startPoint: .leading, endPoint: .trailing))
+                .background(LinearGradient(gradient: Gradient(colors: [Color.brown, Color.yellow]), startPoint: .leading, endPoint: .trailing))
                 .cornerRadius(10)
     }
 }
 
 struct CreditCardView_Previews: PreviewProvider {
     static var previews: some View {
-        CreditCard<CreditCardFront>(content: { CreditCardFront() })
+        CreditCard<CreditCardBack>(content: { CreditCardBack() })
     }
 }
